9ce0080706f538003cf706a6428cf2d7317cd5d0
peeraponglim1125 <peeraponglim1125@users.noreply.github.com>
2025-11-25 18:12:10 +0700

    do ui - close #3

--- student.go
+++ student.go
@@ -2,9 +2,12 @@ package got
 import "time"
 
 type Student struct {	
+
 	ID        int
 	FirstName string
 	LastName  string
 	BirthDate time.Time
 	Email     string
+	Grade     string
+	// Additional fields can be added as needed
 }	
